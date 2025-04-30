25c790a41221e92c120e1d007cd264d710976023
euro000 <euro@chob.marketing>
2025-04-30 11:03:44 +0700

    change index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-export * from './generated'
-import { PrismaClient } from './generated'
+export * from './generated' 
 
-/** Shared Prisma instance (consumer apps can import this) */
+import { PrismaClient } from './generated'
 export const prisma = new PrismaClient()
 
-/** Tiny health-check helper */
 export async function ping() {
   await prisma.$queryRaw`SELECT 1`
   console.log('Prisma is connected 🎉')
